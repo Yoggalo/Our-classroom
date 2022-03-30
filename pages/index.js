4d5f928b81834534e41cf549e49b2530789804f2
Yoggalo <you@example.com>
2022-03-30 22:15:34 +0200

    k

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Layout from './components/layout'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Layout titolo="yey">
+      <Layout titolo="tey">
       </Layout>
     </div>
   )
