5a863345aaa0554a28231ae3900a692ed6774a07
Yoggalo <you@example.com>
2022-03-31 19:00:07 +0200

    home

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,31 @@ export default function Home() {
         <title>Our Classroom</title>{" "}
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <h1>Our Classroom</h1>
+      <h1 className={styles.mainTitle}>Our Classroom</h1>
+      <div className={styles.mainSection}>
+        <h1 className={styles.mainSectionTitle}>Eventi</h1>
+        <div className={styles.mainSectionPart}>
+          Evento 1
+        </div>
+        <div className={styles.mainSectionPart}>
+          Evento 2
+        </div>
+        <div className={styles.mainSectionPart}>
+          Evento 3
+        </div>
+      </div>
+      <div className={styles.mainSection}>
+        <h1 className={styles.mainSectionTitle}>Annunci</h1>
+        <div className={styles.mainSectionPart}>
+          Annuncio 1
+        </div>
+        <div className={styles.mainSectionPart}>
+        Annuncio 2
+        </div>
+        <div className={styles.mainSectionPart}>
+        Annuncio 3
+        </div>
+      </div>
     </div>
   );
 }
